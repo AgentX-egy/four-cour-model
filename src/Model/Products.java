136314131e6dcc0da612b0c619de405ca4b3930b
AbdulRahman Essam Sayed Heikal <starfire19990@gmail.com>
2021-08-18 14:01:01 +0200

    Restructure. Remove extr Library Dependency

---
+++ src/Model/Products.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Products {
+    private int productID;
+    private String name;
+    private int price;
+    private int quantity = 1;
+
+    public Products(int productID, String name, int price) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int checkStock() {
+        return DBConnection.getInstance().getProductQuantity(this.productID);
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public static ArrayList<Products> getAllProducts(){
+        ResultSet rs = DBConnection.getInstance().viewAllProducts();
+        ArrayList<Products> productsList = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                productsList.add(
+                        new Products(rs.getInt(1),rs.getString(2),rs.getInt(3))
+                );
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return productsList;
+    }
+}
